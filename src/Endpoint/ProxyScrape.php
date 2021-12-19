441adf9947e0da33612b2f642ac5dcd9bdceb142
EVOKE <evoke.lektrique@gmail.com>
2021-12-19 22:38:08 +0330

    Update proxyscrape endpoint

--- src/Endpoint/ProxyScrape.php
+++ src/Endpoint/ProxyScrape.php
@@ -7,7 +7,11 @@ use NightVision\Interfaces;
  * Proxyscrape.com API Handler Endpoint
  */
 final class ProxyScrape implements Interfaces\EndpointApi {
-   public function __construct() {
+
+   private $options;
+
+   public function __construct(array $options) {
+      $this->options = $options;
    }
 
    public function get(): array {
@@ -15,6 +19,11 @@ final class ProxyScrape implements Interfaces\EndpointApi {
    }
 
    public function request(): array {
+      $url = $this->generate_request_url($this->options);
       return [];
+   }
+
+   private function generate_request_url($options) {
+      // '?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all'
    }
 }
