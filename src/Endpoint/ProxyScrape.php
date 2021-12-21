c18e1086a54fea73cd3577f68268dc0aacebaf5f
EVOKE <evoke.lektrique@gmail.com>
2021-12-21 16:46:11 +0330

    Update `ProxyScrape` endpoint

--- src/Endpoint/ProxyScrape.php
+++ src/Endpoint/ProxyScrape.php
@@ -1,7 +1,12 @@
 <?php
 
 namespace NightVision\Endpoint;
+
 use NightVision\Interfaces;
+use NightVision\Option;
+use Psr\Http\Message\ResponseInterface;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Client;
 
 /**
  * Proxyscrape.com API Handler Endpoint
@@ -9,21 +14,70 @@ use NightVision\Interfaces;
 final class ProxyScrape implements Interfaces\EndpointApi {
 
    private $options;
+   private $client;
+   private $defaults;
+
+   const V2_BASE_URL = "https://api.proxyscrape.com/v2/";
 
    public function __construct(array $options) {
-      $this->options = $options;
+      $this->options = $this->parse_options($options);
+      $this->client = new Client(["base_uri" => self::V2_BASE_URL]);
    }
 
    public function get(): array {
-      return [];
+      $body = $this->request();
+
+      return $body;
    }
 
    public function request(): array {
       $url = $this->generate_request_url($this->options);
-      return [];
+      $response = $this->client->request("GET", "?" . $url);
+      $body = (string) $response->getBody();
+
+      return explode(PHP_EOL, $body);
    }
 
-   private function generate_request_url($options) {
-      // '?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all'
+   private function generate_request_url($options): string {
+      return http_build_query($options);
+   }
+
+   private function get_defaults(): array {
+      $defaults = [];
+      $defaults["timeout"] = 1000;
+      $defaults["protocol"] = "http";
+      $defaults["country"] = "all";
+      $defaults["ssl"] = "all";
+      $defaults["anonymity"] = "all";
+
+      return $defaults;
+   }
+
+   private function parse_options(array $options): array {
+      $defaults = $this->get_defaults();
+
+      $options["request"] = "displayproxies";
+
+      if(!isset($options["timeout"]) || empty($options["timeout"])) {
+         $options["timeout"] = $defaults["timeout"];
+      }
+
+      if(!isset($options["protocol"]) || empty($options["protocol"])) {
+         $options["protocol"] = $defaults["protocol"];
+      }
+
+      if(!isset($options["country"]) || empty($options["country"])) {
+         $options["country"] = $defaults["country"];
+      }
+
+      if(!isset($options["ssl"]) || empty($options["ssl"])) {
+         $options["ssl"] = $defaults["ssl"];
+      }
+
+      if(!isset($options["anonymity"]) || empty($options["anonymity"])) {
+         $options["anonymity"] = $defaults["anonymity"];
+      }
+
+      return $options;
    }
 }
